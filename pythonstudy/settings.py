0e2995e3486241615ae02f37f32e9c1ec15a4f3c
星龙剑 <xinglongjian@qq.com>
2014-11-15 02:49:17 +0000

    use mysql database

--- pythonstudy/settings.py
+++ pythonstudy/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'pythonstudy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'c9',
+        'USER': 'xinglongjian1',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
